078251ae7ad3c1eeabcdc79bac9a78f7a0ab45ff
Jim Montgomery <github@jimmont.com>
2022-03-12 14:07:08 +0200

    adjust so that a specific section loads its own respective translation separate from the overall project and stays in sync with the users selected locale

--- www/page-common.js
+++ www/page-common.js
@@ -1,25 +1,63 @@
 import { ReactiveElement, LitElement, html, svg, css } from '/wildtype/lit.js';
 import { msg, updateWhenLocaleChanges, getLocale, } from './locales.js';
+import { until } from '/wildtype/lit/directives/until.js';
 
+/* use a component across several urls as an example for similar views, with localization/translation */
 customElements.define('page-common', class extends LitElement{
+	static get properties(){
+		return {
+			translation: {type: Object},
+			loading: {type: Boolean},
+			request: {type: Promise},
+			lang: {type: String},
+		}
+	}
+	static get styles(){
+		return css`
+			:host{
+				display: contents;
+			}
+		`;
+	}
 	constructor(){
 		super();
 		updateWhenLocaleChanges(this);
+		this.localize = this.localize.bind(this);
 	}
 	createRenderRoot() {
 		return this;
 	}
+	localize({type, detail}){
+		const lang = getLocale();
+		if(lang === this.lang) return;
+		this.lang = lang;
+		this.loading = true;
+		this.translation = html`loading`;
+		this.request = import(`/page.${ getLocale() }.js`).then(res=>{
+			this.translation = res.templates[ location.pathname ];
+			return res;
+		}).catch(err=>{
+			this.translation = html`<b>error</b> ${ err }`;
+			return Promise.reject(err);
+		})
+		.finally(()=>{ this.loading = false; })
+		;
+	}
 	connectedCallback(){
 		super.connectedCallback();
-		const { search, searchParams, hash, pathname } = new URL(location);
-		console.warn('page...', { search, searchParams, hash, pathname });
+		this.localize({type: 'connectedCallback-localize-init'});
+		self.addEventListener('lit-localize-status', this.localize);
+	}
+	disconnectedCallback(){
+		self.removeEventListener('lit-localize-status', this.localize);
 	}
 	render(){
-		console.log('render...page', getLocale());
 		// replace specific to demo
 		const id = location.pathname.replace('starter/www/', '');
-		return html`page... "${ id }" ${ msg('...', {id, desc: 'content for page by pathname'}) }
-${ msg('text', {id: 'demo.text.sample', desc: 'some text to show'}) }
-		`;
+		if(id === '/'){
+			return html`page... "${ id }" ${ msg('...', {id: 'lang'}) } ${ msg('####', {id: 'demo.text.sample'}) }`;
+		}else{
+			return html`page... "${ id }" ${ until(this.translation, html`...loading TODO localize ${ msg('...', {id:'lang'}) } text`) }`;
+		}
 	}
 });
--- www/sour-part.js
+++ www/sour-part.js
@@ -1,7 +1,7 @@
 import { ReactiveElement, LitElement, html, svg, css } from '/wildtype/lit.js';
 import { msg, updateWhenLocaleChanges, getLocale, } from './locales.js';
 /* sour-part exists for translating and localizing content where another component doesn't exist */
-customElements.define('sour-part', class SourLocale extends LitElement{
+customElements.define('sour-part', class SourPart extends LitElement{
 	static get properties(){
 		return {
 			msg: {type: String}
