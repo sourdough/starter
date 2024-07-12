44b3a66a557cda26e2d2769465dc85e5735d0a43
Jim Montgomery <github@jimmont.com>
2024-07-12 14:04:50 +0800

    add overlooked missing export userLocale

--- www/deps.js
+++ www/deps.js
@@ -1,10 +1,10 @@
 import { ReactiveElement, LitElement, html, svg, css } from 'https://sourdough.github.io/wildtype/lit/index.js';
-import { msg, updateWhenLocaleChanges, localeList, localeDictionary, setLocale, getLocale, updateLocaleFromUrl, } from './locales.js';
+import { msg, updateWhenLocaleChanges, localeDictionary, localeList, getLocale, setLocale, userLocale, updateLocaleFromUrl } from '/locales.js';
 import { until } from 'https://sourdough.github.io/wildtype/lit/directives/until.js';
 import { render } from 'https://sourdough.github.io/wildtype/lit/html.js';
 
 export { ReactiveElement, LitElement, html, svg, css, 
-	msg, updateWhenLocaleChanges, localeList, localeDictionary, setLocale, getLocale, updateLocaleFromUrl, 
+	msg, updateWhenLocaleChanges, localeDictionary, localeList, getLocale, setLocale, userLocale, updateLocaleFromUrl,
 	until,
 	render
 };
