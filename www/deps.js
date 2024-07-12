65ff2ee518db836991683308435fbddd87a4b0a9
Jim Montgomery <github@jimmont.com>
2024-07-12 14:08:23 +0800

    fix path

--- www/deps.js
+++ www/deps.js
@@ -1,5 +1,5 @@
 import { ReactiveElement, LitElement, html, svg, css } from 'https://sourdough.github.io/wildtype/lit/index.js';
-import { msg, updateWhenLocaleChanges, localeDictionary, localeList, getLocale, setLocale, userLocale, updateLocaleFromUrl } from '/locales.js';
+import { msg, updateWhenLocaleChanges, localeDictionary, localeList, getLocale, setLocale, userLocale, updateLocaleFromUrl } from './locales.js';
 import { until } from 'https://sourdough.github.io/wildtype/lit/directives/until.js';
 import { render } from 'https://sourdough.github.io/wildtype/lit/html.js';
 
